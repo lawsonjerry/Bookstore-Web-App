7e5faff387896a3d8186589c2a1ed3a732279eaf
ada lovelace <williambookman@pursuit.org>
2023-11-09 20:50:16 -0500

    adding header and hamburger menu

---
+++ main.js
@@ -0,0 +1,8 @@
+// const searchBar = document.getElementById('search-bar');
+
+// //a pop up window if someone is trying to use the search bar
+// searchBar.addEventListener('keyup', (event) => {
+//     if (event.keyCode === 13) {
+//   alert('The search bar is still a work in progress.');
+// }
+// });
