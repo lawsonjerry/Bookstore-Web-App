5ea7913925d7d06fa97a0d133c6d46420b12cdc6
ada lovelace <williambookman@pursuit.org>
2023-11-10 05:34:14 -0500

    started builing functionality make bookcard

--- main.js
+++ main.js
@@ -6,3 +6,20 @@
 //   alert('The search bar is still a work in progress.');
 // }
 // });
+
+function createBookCard(book) {
+  const mainSection = document.querySelector('main');
+
+
+  //container for the bookCard Element
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("bookcard");
+  mainSection.append(bookCard)
+
+  const bookImageElement = document.createElement("img");
+  bookImageElement.id = ("wont-get-fooled")
+  bookImageElement.setAttribute("src", 'Assets/wont_get_fooled_again_bookCover.jpg')
+  bookCard.append(bookImageElement)
+}
+
+createBookCard(null);
